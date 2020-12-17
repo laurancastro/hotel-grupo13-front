cf6326c2519d72b2e525b9cc1d1b4b221ae9f63c
laurancastro <laura.castrogarcia57@gmail.com>
2020-12-17 09:27:31 -0500

    mensaje 2

--- src/components/VerReserva.vue
+++ src/components/VerReserva.vue
@@ -47,7 +47,7 @@
             this.idReserva = this.$route.params.idReserva
             let self = this
       
-            axios.get("http://localhost:8000/reserva/" + this.idReserva)
+            axios.get("https://hotel-api-grupo13.herokuapp.com//reserva/" + this.idReserva)
                 .then((result) => {
                     self.idReserva = result.data.idReserva
                     self.fechaReserva = result.data.fechaReserva
